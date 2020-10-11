e5d69c7b98b214a47a0a06f068028ce1e5a6dd35
Matan Perlmutter <matan@deepchecks.com>
2020-10-11 13:16:09 +0300

    Change window functions

--- init.lua
+++ init.lua
@@ -8,12 +8,58 @@ hyper:bind({"cmd"}, "s", function()
   hs.caffeinate.systemSleep()
 end)
 
+function windowResizeFrames(dir, screen)
+  local cres = screen:fullFrame()
+  local topbar_diff = cres.h - screen:frame().h
+  if dir == 'left' then
+    return {x=cres.x, y=cres.y + topbar_diff, w=cres.w/2, h=cres.h - topbar_diff}
+  elseif dir == 'right' then
+    return {x=cres.x+cres.w/2, y=cres.y + topbar_diff, w=cres.w/2, h=cres.h - topbar_diff}
+  elseif dir == 'down' then
+    return {x=cres.x, y=cres.y+cres.h/2, w=cres.w, h=cres.h/2}
+  end
+end
+
 -- shortcut for window resize
-hyper:bind({}, "right", function() winwin:moveAndResize('halfright') end, nil, function() winwin:moveToScreen('right') end)
-hyper:bind({}, "left", function() winwin:moveAndResize('halfleft') end, nil, function() winwin:moveToScreen('left') end)
+hyper:bind({}, "right", function()
+  local cwin = hs.window.focusedWindow()
+  if cwin then
+      local cscreen = cwin:screen()
+      local wf = cwin:frame()
+      local geo = windowResizeFrames('right', cscreen)
+      if wf:floor():equals(geo) then
+        cwin:setFrame(windowResizeFrames('left', cscreen:next()))
+      else
+        cwin:setFrame(geo)
+      end
+  end
+end)
+hyper:bind({}, "left", function()
+  local cwin = hs.window.focusedWindow()
+  if cwin then
+      local cscreen = cwin:screen()
+      local wf = cwin:frame()
+      local geo = windowResizeFrames('left', cscreen)
+      if wf:floor():equals(geo) then
+        cwin:setFrame(windowResizeFrames('right', cscreen:previous()))
+      else
+        cwin:setFrame(geo)
+      end
+  end
+end)
 hyper:bind({}, "up", function() winwin:moveAndResize('maximize') end)
 hyper:bind({}, 'down',  function() winwin:moveAndResize('halfdown') end, nil, function() winwin:moveAndResize('halfup') end)
 hyper:bind({}, "return", function() hs.window.focusedWindow():toggleFullScreen() end)
+hyper:bind({"cmd"}, "left", function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect
+  win:move(win:frame():toUnitRect(screen:frame()), screen:previous(), true, 0)
+end)
+hyper:bind({"cmd"}, "right", function() winwin:moveToScreen('right') end)
 
 -- shortcut to show desktop
 hyper:bind({}, "delete", function()
